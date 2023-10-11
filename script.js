17e194d5865f4fb8f1c382535bd68e3a19644639
Freja Fischer <frej657e@stud.kea.dk>
2023-10-11 12:05:58 +0200

    form in the making

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-import { getPets } from "./utils/REST_pets.js";
+import { deletePet, getPets, patchPet } from "./utils/REST_pets.js";
 
 async function displayPets() {
   const pets = await getPets();
   console.log(pets);
+  document.querySelector(".pets").innerHTML = "<article><h2>Add new pet</h2><button>Add</button></article>";
 
   pets.forEach((pet) => {
     const clone = document.querySelector("template#pet").content.cloneNode(true);
@@ -14,6 +15,20 @@ async function displayPets() {
     clone.querySelector("[data-field=act]").textContent = pet.activityLevel;
     clone.querySelector("[data-field=dob]").textContent = pet.dob;
     clone.querySelector("[data-field=alive]").textContent = pet.isAlive;
+
+    const deleteBtn = clone.querySelector("button[data-action=delete]");
+    deleteBtn.dataset.id = pet.id;
+    const updateBtn = clone.querySelector("button[data-action=update]");
+    updateBtn.dataset.id = pet.id;
+
+    deleteBtn.addEventListener("click", async (e) => {
+      await deletePet(pet.id);
+      displayPets();
+    });
+    updateBtn.addEventListener("click", async (e) => {
+      await patchPet(pet.id);
+      displayPets();
+    });
 
     document.querySelector(".pets").appendChild(clone);
   });
